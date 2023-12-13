ee1c9c2c15bc9d56507a25ee0f2e467af867263b
Wireless Dan <wirelessdan@proton.me>
2023-12-12 21:42:50 -0500

    undelete

--- action.sh
+++ action.sh
@@ -1,6 +1,4 @@
 #/bin/bash
 
-rm -rf update_dtrip
-git clone https://github.com/skurrrrr/update_dtrip
 cp update.sh /root/update.sh
-touch /root/test.txt
+touch test.txt
---
+++ update.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+cd /root
+rm -rf update_dtrip
+git clone https://github.com/skurrrrr/update_dtrip
