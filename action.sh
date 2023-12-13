56ad1a8ceaa39678a99b9c01e728c8aac921bda4
Wireless Dan <wirelessdan@proton.me>
2023-12-12 21:39:22 -0500

    test.txt

--- action.sh
+++ action.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-
-git clone https://github.com/skurrrrr/update_dtrip/tree/main
+rm -rf update_dtrip
+git clone https://github.com/skurrrrr/update_dtrip
 cp update.sh /root/update.sh
 touch /root/test.txt
